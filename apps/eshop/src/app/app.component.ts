80ecbaa16f963d8b89a2e901c54d4fecbec88947
MohamedenVal <med4en@gmail.com>
2021-09-22 01:01:40 +0000

    added base sckeleton for the app and some reusable libs

--- apps/eshop/src/app/app.component.ts
+++ apps/eshop/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'agsa-shop-root',
+  selector: 'eshop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
